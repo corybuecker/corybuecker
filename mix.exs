ad0277b3b09d0926e540fad5994cd023c2f46714
Cory Buecker <email@corybuecker.com>
2022-03-12 14:47:58 -0600

    feat: add Twitter image

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blog.MixProject do
     [
       app: :blog,
       version: "1.0.0",
-      elixir: "1.13.2",
+      elixir: "1.13.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
