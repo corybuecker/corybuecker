721209ecb9079f488b38faef3969dfd3c1800c13
Cory Buecker <email@corybuecker.com>
2024-05-21 19:39:29 -0500

    move all content into Mongo

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -5,6 +5,9 @@ import elixir from 'highlight.js/lib/languages/elixir'
 import javascript from 'highlight.js/lib/languages/javascript'
 import bash from 'highlight.js/lib/languages/bash'
 import yaml from 'highlight.js/lib/languages/yaml'
+import dockerfile from 'highlight.js/lib/languages/dockerfile'
+import nginx from 'highlight.js/lib/languages/nginx'
+import css from 'highlight.js/lib/languages/css'
 
 declare global {
     interface Window { htmx: any; }
@@ -14,7 +17,17 @@ hljs.registerLanguage('elixir', elixir)
 hljs.registerLanguage('javascript', javascript)
 hljs.registerLanguage('bash', bash)
 hljs.registerLanguage('yaml', yaml)
+hljs.registerLanguage('dockerfile', dockerfile)
+hljs.registerLanguage('nginx', nginx)
+hljs.registerLanguage('css', css)
 
 hljs.highlightAll()
 
 window.htmx = htmx;
+
+const timeElements: HTMLCollectionOf<HTMLTimeElement> = document.getElementsByTagName('time')
+
+for (const el of timeElements) {
+    const timeString = el.dateTime
+    el.innerText = new Date(timeString).toLocaleDateString()
+}
